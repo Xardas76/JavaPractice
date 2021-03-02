fe65e1025da912ca49dd89ad366c73ce9ff4a2f5
Ivan <ivan.diachenko.chr@gmail.com>
2021-03-02 11:53:07 -0500

    commit pr2

--- Practice 2/Test.java
+++ Practice 2/Test.java
@@ -3,6 +3,7 @@ package com.company;
 public class Test {
 
     public static void main(String[] args) {
+        System.out.println("Yes, we start tests now");
         Ball b1 = new Ball(100, 100);
         System.out.println(b1);
         b1.move(30, 15);
